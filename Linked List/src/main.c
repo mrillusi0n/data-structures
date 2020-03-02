d823dc95516965a750179f170c8f4e38c4188587
Nikhil <csbnikhil@gmail.com>
2020-03-02 18:46:12 +0530

    Restructured

--- Linked List/src/main.c
+++ Linked List/src/main.c
@@ -13,12 +13,12 @@ int main() {
 
     head = get_node(0);
 
-    add_last(head, get_node(3));
+    add_last(head, get_node(396));
     add_last(head, get_node(9));
     delete_first(head);
     add_last(head, get_node(8));
     delete_last(head);
-    add_first(head, get_node(16));
+    add_first(head, get_node(106));
     add_last(head, get_node(7));
 
     display_nodes(head);
